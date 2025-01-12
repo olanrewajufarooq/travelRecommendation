818e4f19bd6027e0d049c32716249c1404e1cd95
Farooq Olanrewaju <olanrewajufarooq@yahoo.com>
2025-01-12 14:35:15 -0500

    [EDIT]: Adding log info.

--- assets/js/main_script.js
+++ assets/js/main_script.js
@@ -1,5 +1,6 @@
-
+console.log("Starting JS Script");
 fetch('../json/travel_recommendation_api.json')
   .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.error('Error:', error));
+  console.log("Ending JS Script");
